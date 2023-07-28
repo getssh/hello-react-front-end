49f673ed830bce31b18ff80614ea964b47001258
getssh <gtamrat33@gmail.com>
2023-07-28 15:46:47 +0300

    Add procider to index

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getGreet } from '../redux/greetSlice';
 
 function Greeting() {
+  const message = useSelector((state) => state.greeting.message);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(getGreet());
+  }, []);
+
   return (
-    <div>Greeting</div>
+    <div>
+      <h1>{message.greeting}</h1>
+    </div>
   );
 }
 
